77f6ad9c3255d04c8bc5d5120d9c87b5a9259942
Scout <Scout.a.miller@gmail.com>
2026-01-01 19:07:39 -0800

    version text

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,12 +3,14 @@ extends Control
 var Level1
 var Level2
 var Level3
+@export var versionText: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Level1 = preload("res://Scenes/quick_cast_main.tscn")
 	Level2 = preload("res://Scenes/manual_cast_main.tscn")
 	Level3 = preload("res://Scenes/instant_manual_cast_main.tscn")
+	versionText.text = ProjectSettings.get_setting("application/config/version")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
