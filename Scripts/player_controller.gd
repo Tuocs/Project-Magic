4518cee1c6526e4e95f94692cd78e5fac4d1ac8b
Scout <Scout.a.miller@gmail.com>
2026-05-12 16:44:52 -0700

    fix mod overlap

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -123,12 +123,12 @@ func base_spell_effects(scene: PackedScene, pos: Vector3, rot) -> Node3D:
 		spwn.damage = spwn.damage + 50
 	if spell_mods[Globals.spell_mod.DURATION_UP]:
 		spwn.lifetime = spwn.lifetime*2
-	if spell_mods[Globals.spell_mod.DURATION_DOWN]:
-		spwn.lifetime = spwn.lifetime/3
+	#if spell_mods[Globals.spell_mod.DURATION_DOWN]:
+		#spwn.lifetime = spwn.lifetime/3
 	if spell_mods[Globals.spell_mod.PUSH]:
 		spwn.knockback = 8
-	if spell_mods[Globals.spell_mod.PULL]:
-		spwn.knockback = -8
+	#if spell_mods[Globals.spell_mod.PULL]:
+		#spwn.knockback = -8
 	return spwn
 
 func base_spell_costs() -> bool:
